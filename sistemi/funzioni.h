553bf195225969f10d9be7a227f91e26be6398ed
D4rio283 <165889042+D4rio283@users.noreply.github.com>
2025-02-05 21:43:02 +0100

    Add files via upload

---
+++ sistemi/funzioni.h
@@ -0,0 +1,27 @@
+#ifndef FUNZIONI_H
+#define FUNZIONI_H
+#include<string>
+using namespace std;
+struct IpHeader{
+    int version;
+    int HLEN;
+    int TOS;
+    int total_lenght;
+    int identification;
+    int flags;
+    int fragment_offset;
+    int time_to_live;
+    int protocol;
+    int header_checksum;
+    int S_IP_address;
+    int D_IP_address;
+    int option;
+    int padding;
+};
+
+string leggi_Messaggio();
+void scrittura_Messaggio(string messaggio);
+string char_to_bin(char lettera);
+void configurazione_header(IpHeader &header);
+
+#endif	//serve a evitare inclusioni multiple
