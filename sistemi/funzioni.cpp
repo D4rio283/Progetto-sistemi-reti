11e76bd9f9cf4ed8cc33538320580210d097134c
Dario <165889042+D4rio283@users.noreply.github.com>
2025-02-28 10:06:55 +0100

    Update funzioni.cpp

--- sistemi/funzioni.cpp
+++ sistemi/funzioni.cpp
@@ -10,7 +10,7 @@ string leggi_Messaggio(){
     string riga;
     
     //apro lo stream col file
-    ifstream fin("msg.txt");
+    ifstream fin("C://Users//DRIDI.DARIO//Documents//projects//Project1//msg.txt");
     
     //se c'e' un errore nell'apertura lo comunico all'utente e torno al main
     if(!fin){
@@ -30,7 +30,7 @@ string leggi_Messaggio(){
 
 void scrittura_Messaggio(string messaggio){
     //apro lo stream con il file
-    ofstream fout("frame.txt");
+    ofstream fout("C://Users//DRIDI.DARIO//Documents//projects//Project1//frame.txt");
     
     //se c'e' un errore nell'apertura lo comunico all'utente e torno al main
     if(!fout){
@@ -132,14 +132,14 @@ string string_to_bin(char lettera){
     return risultato;
 }
 
-void crea_Frame(ethernetHeader &header2,string risultato_ip){
+string crea_Frame(ethernetHeader &header2, string risultato_ip){
     string risultato_totale="";
     header2.preamble="1010101010101010101010101010101010101010101010101010101010101010";
     header2.SFD="11010101";
     header2.DST="101011111010111110101111101011111010111110101111";
     header2.SRC="101011001010111110101101101011001010011010101110";
     header2.type="10000000000000000";
-    header2.FCS=calcolaCRC(risultato_ip);
+    header2.FCS= calcolaCRC(risultato_ip);
     risultato_totale=header2.preamble+header2.SFD+header2.DST+header2.SRC+header2.type+risultato_ip+header2.FCS;
     return risultato_totale;
    
@@ -161,10 +161,10 @@ unsigned short ipChecksum(string bits){
 
 string calcolaCRC(const string &payload) {
     unsigned short crc = 0xFFFF;  // Inizializzazione CRC a 0xFFFF
-    const unsigned short POLY = 0x1021;  // Polinomio CRC-16
+    const unsigned short POLY = 0x8005;  // Polinomio CRC-16
 
     // Loop attraverso ogni carattere del payload
-    for (unsigned char c : payload) {
+    for (int c=0; c<payload.size(); c++) {
         crc ^= (c << 8);  // XOR con il byte corrente, spostato a sinistra di 8 bit
         for (int i = 0; i < 8; i++) {  // Loop per i 8 bit
             if (crc & 0x8000)  // Se il bit più significativo è 1
@@ -187,6 +187,14 @@ string calcolaCRC(const string &payload) {
 
     return result;  // Restituisce il CRC come stringa esadecimale
 }
+
+
+
+
+
+
+
+
 
 
 
